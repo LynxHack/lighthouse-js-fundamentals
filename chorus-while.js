59bda1cb4b661d9d41553c585006dd8dc25aadbc
LynxHack <KevinLynx_L@hotmail.com>
2018-05-24 03:35:41 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,22 @@
+//while loop syntax
+
+var chorus = "Let's dance!";
+var repeat =0;
+
+while (repeat < 10) {
+	if (repeat === 5) {
+		console.log("*change key*");
+	}
+	console.log(chorus);
+	repeat++;
+}
+
+console.log("Until the sun comes up!");
+
+//For loop syntax
+
+var chorus = "let's dance!";
+for (var repeat = 0; repeat < 10; repeat++){
+	console.log(chorus);
+}
+console.log("Until the sun comes up!");
